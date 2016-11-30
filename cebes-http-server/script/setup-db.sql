6a3d16d641db1baf1b6bbffd81f0fb4f9f0a1592
Vu Pham <phvu225@gmail.com>
2016-11-30 18:05:25 +0100

    updated jdbc creds in configuration files

--- cebes-http-server/script/setup-db.sql
+++ cebes-http-server/script/setup-db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE cebes_test_db;
 CREATE DATABASE cebes_store;
-CREATE DATABASE hive_metastore;
+CREATE DATABASE cebes_hive_metastore;
 
-GRANT ALL PRIVILEGES ON hive_metastore.* TO 'cebes_hive' IDENTIFIED BY 'cebes_hive_pwd';
-GRANT ALL PRIVILEGES ON cebes_test_db.* TO 'cebes' IDENTIFIED BY 'cebespwd';
-GRANT ALL PRIVILEGES ON cebes_store.* TO 'cebes';
+GRANT ALL PRIVILEGES ON cebes_hive_metastore.* TO 'cebes_hive' IDENTIFIED BY 'cebes_hive_pwd';
+GRANT ALL PRIVILEGES ON cebes_test_db.* TO 'cebes_server' IDENTIFIED BY 'cebes_server_pwd';
+GRANT ALL PRIVILEGES ON cebes_store.* TO 'cebes_server';
