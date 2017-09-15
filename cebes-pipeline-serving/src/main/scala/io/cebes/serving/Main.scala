6702181fde73736a99e931d73995299247b0ae6e
phvu <phvu225@gmail.com>
2017-09-15 18:10:30 +0200

    initial cebes pipeline serving

---
+++ cebes-pipeline-serving/src/main/scala/io/cebes/serving/Main.scala
@@ -0,0 +1,25 @@
+/* Copyright 2016 The Cebes Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, version 2.0 (the "License").
+ * You may not use this work except in compliance with the License,
+ * which is available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+package io.cebes.serving
+
+import io.cebes.http.server.HttpServer
+import io.cebes.serving.inject.CebesServingInjector
+
+object Main {
+
+  def main(args: Array[String]) {
+    val server = CebesServingInjector.instance[HttpServer]
+    server.start()
+    server.waitServer()
+    server.stop()
+  }
+}
