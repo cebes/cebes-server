9530145cfd327f36c033aec8bd9eb06312bea6bd
Vu Pham <phvu225@gmail.com>
2018-02-04 01:57:44 +0000

    push version to 0.10.0

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-VERSION_TAG=${1:-"0.10.0-snapshot"}
-HTTP_SERVER_TAG="phvu/cebes-private:server-${VERSION_TAG}"
-SERVING_TAG="phvu/cebes-private:serving-${VERSION_TAG}"
-REPOSITORY_TAG="phvu/cebes-private:repo-${VERSION_TAG}"
+set -e
+
+VERSION_TAG=${1:-"0.10.0"}
+HTTP_SERVER_TAG="cebesio/http-server:${VERSION_TAG}"
+SERVING_TAG="cebesio/pipeline-serving:${VERSION_TAG}"
+REPOSITORY_TAG="cebesio/pipeline-repo:${VERSION_TAG}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}/..
-sbt clean compile package assembly || exit
+sbt clean compile package assembly
 
 cd ${DIR}
 mkdir -p binaries
@@ -18,23 +20,23 @@ cp ../cebes-pipeline-repository/target/scala-2.11/cebes-pipeline-repository-asse
 
 for maria_db in true false; do
 
-    server_tag="${HTTP_SERVER_TAG}"
-    if [ "x$maria_db" = "xtrue" ]; then server_tag="${HTTP_SERVER_TAG}-mariadb"; fi
-    echo "Building and pushing ${server_tag}"
-    docker build -t ${server_tag} -f http-server/Dockerfile --build-arg WITH_MARIADB=${maria_db} . || exit
-    docker push ${server_tag}
+    db_tag=""
+    if [ "x$maria_db" = "xfalse" ]; then db_tag="-no-mariadb"; fi
 
-    serving_tag="${SERVING_TAG}"
-    if [ "x$maria_db" = "xtrue" ]; then serving_tag="${SERVING_TAG}-mariadb"; fi
-    echo "Building and pushing ${serving_tag}"
-    docker build -t ${serving_tag} -f serving/Dockerfile --build-arg WITH_MARIADB=${maria_db} . || exit
-    docker push ${serving_tag}
+    image_tag="${HTTP_SERVER_TAG}${db_tag}"
+    echo "Building and pushing ${image_tag}"
+    docker build -t ${image_tag} -f http-server/Dockerfile --build-arg WITH_MARIADB=${maria_db} .
+    docker push ${image_tag}
 
-    repo_tag="${REPOSITORY_TAG}"
-    if [ "x$maria_db" = "xtrue" ]; then repo_tag="${REPOSITORY_TAG}-mariadb"; fi
-    echo "Building and pushing ${repo_tag}"
-    docker build -t ${repo_tag} -f repository/Dockerfile --build-arg WITH_MARIADB=${maria_db} . || exit
-    docker push ${repo_tag}
+    image_tag="${SERVING_TAG}${db_tag}"
+    echo "Building and pushing ${image_tag}"
+    docker build -t ${image_tag} -f serving/Dockerfile --build-arg WITH_MARIADB=${maria_db} .
+    docker push ${image_tag}
+
+    image_tag="${REPOSITORY_TAG}${db_tag}"
+    echo "Building and pushing ${image_tag}"
+    docker build -t ${image_tag} -f repository/Dockerfile --build-arg WITH_MARIADB=${maria_db} .
+    docker push ${image_tag}
 done
 
 rm -r binaries
